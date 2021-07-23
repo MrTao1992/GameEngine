efe9891c3037bd8d645343dbcb80b68ef8d85ec4
MrTao1992 <2247121604@qq.com>
2021-07-23 17:20:12 +0800

    #Delete the window platform initialization code

--- GameEngine/Platform/Windows/WindowsApplication.h
+++ GameEngine/Platform/Windows/WindowsApplication.h
@@ -30,9 +30,3 @@ namespace Phantom
     };
 
 }
-namespace Phantom
-{
-    GfxConfiguration config(8, 8, 8, 8, 32, 0, 0, 960, 540, ("Game Engine From Scratch (Windows)"));
-    IApplication *m_app = static_cast<IApplication *>(new WindowsApplication(config));
-    GraphicsManager *m_graphicsManager = static_cast<GraphicsManager *>(new GraphicsManager());
-}
