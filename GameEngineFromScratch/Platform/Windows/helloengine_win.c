4699cb4b5d44a24925a6c386204c66e0da82afb8
YUQI\taoqiyong <taoqiyong@gg.com>
2021-08-23 14:48:07 +0800

    #Draw up the simple graphics on the screen

--- GameEngineFromScratch/Platform/Windows/helloengine_win.c
+++ GameEngineFromScratch/Platform/Windows/helloengine_win.c
@@ -2,12 +2,12 @@
 #include <windows.h>
 #include <windowsx.h>
 #include <tchar.h>
-
+#pragma comment(lib, "gdi32.lib")
 // the WindowProc function prototype
 LRESULT CALLBACK WindowProc(HWND hWnd,
-                         UINT message,
-                         WPARAM wParam,
-                         LPARAM lParam);
+                            UINT message,
+                            WPARAM wParam,
+                            LPARAM lParam);
 
 // the entry point for any Windows program
 int WINAPI WinMain(HINSTANCE hInstance,
@@ -37,17 +37,17 @@ int WINAPI WinMain(HINSTANCE hInstance,
 
     // create the window and use the result as the handle
     hWnd = CreateWindowEx(0,
-                          _T("WindowClass1"),    // name of the window class
-                          _T("Hello, Engine!"),   // title of the window
-                          WS_OVERLAPPEDWINDOW,    // window style
-                          300,    // x-position of the window
-                          300,    // y-position of the window
-                          500,    // width of the window
-                          400,    // height of the window
-                          NULL,    // we have no parent window, NULL
-                          NULL,    // we aren't using menus, NULL
-                          hInstance,    // application handle
-                          NULL);    // used with multiple windows, NULL
+                          _T("WindowClass1"),   // name of the window class
+                          _T("Hello, Engine!"), // title of the window
+                          WS_OVERLAPPEDWINDOW,  // window style
+                          300,                  // x-position of the window
+                          300,                  // y-position of the window
+                          500,                  // width of the window
+                          400,                  // height of the window
+                          NULL,                 // we have no parent window, NULL
+                          NULL,                 // we aren't using menus, NULL
+                          hInstance,            // application handle
+                          NULL);                // used with multiple windows, NULL
 
     // display the window on the screen
     ShowWindow(hWnd, nCmdShow);
@@ -58,7 +58,7 @@ int WINAPI WinMain(HINSTANCE hInstance,
     MSG msg;
 
     // wait for the next message in the queue, store the result in 'msg'
-    while(GetMessage(&msg, NULL, 0, 0))
+    while (GetMessage(&msg, NULL, 0, 0))
     {
         // translate keystroke messages into the right format
         TranslateMessage(&msg);
@@ -75,18 +75,34 @@ int WINAPI WinMain(HINSTANCE hInstance,
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     // sort through and find what code to run for the message given
-    switch(message)
+    switch (message)
     {
-        // this message is read when the window is closed
-        case WM_DESTROY:
-            {
-                // close the application entirely
-                PostQuitMessage(0);
-                return 0;
-            } break;
+    case WM_PAINT:
+    {
+        //绘制状态
+        PAINTSTRUCT ps;
+        //设备场景句柄
+        HDC hdc = BeginPaint(hWnd, &ps);
+        //绘制区域
+        RECT rec = {20, 20, 50, 80};
+        //画笔刷子（GetStockObject需要添加链接库libgdi.lib）
+        HGDIOBJ brush = GetStockObject(BLACK_BRUSH);
+        //开始填充绘制区域
+        FillRect(hdc, &rec, (HBRUSH)brush);
+        //结束绘制状态
+        EndPaint(hWnd, &ps);
+    }
+    break;
+    // this message is read when the window is closed
+    case WM_DESTROY:
+    {
+        // close the application entirely
+        PostQuitMessage(0);
+        return 0;
+    }
+    break;
     }
 
     // Handle any messages the switch statement didn't
-    return DefWindowProc (hWnd, message, wParam, lParam);
+    return DefWindowProc(hWnd, message, wParam, lParam);
 }
-
